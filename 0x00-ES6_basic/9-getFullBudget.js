55dfe013c9306081c7ec2c17e584bbc30cbed113
NyaxDa <nyaxda@gmail.com>
2024-07-22 15:47:36 -0700

    Errorfix on File 9

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -4,12 +4,8 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars: () => {
-      return `$${budget.income}`;
-    },
-    getIncomeInEuros: () => {
-      return `${budget.income} euros`;
-    },
+    getIncomeInDollars: () => `$${budget.income}`,
+    getIncomeInEuros: () => `${budget.income} euros`,
   };
 
   return fullBudget;
