3041135c664ce92b92c572caa236098a185f2eb4
NyaxDa <nyaxda@gmail.com>
2024-07-23 16:45:11 -0700

    Errorfix on File 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function handleProfileSignup() {
-  try {
-    const [photoData, userData] = await Promise.all([uploadPhoto(), createUser()]);
-    console.log(photoData.body, userData.firstName, userData.lastName);
-  } catch (error) {
-    console.error('Signup system offline');
-  }
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+    })
+    .catch(() => {
+      console.error('Signup system offline');
+    });
 }
