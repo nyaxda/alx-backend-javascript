df1e08aa07ebc80a78da9e176af4204d7869551a
NyaxDa <nyaxda@gmail.com>
2024-07-23 15:58:51 -0700

    Errorfix on File 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,19 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (promise) {
-        resolve({ status: 200, body: 'Success' });
-      } else {
-        reject(new Error());
-      }
-    }, 1000);
-  }).then((response) => {
-    console.log('Got a response from the API');
-    console.log(response);
-    return response;
-  }).catch((error) => {
-    console.log('Got a response from the API');
-    console.error(error);
-    throw error;
-  });
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
 }
