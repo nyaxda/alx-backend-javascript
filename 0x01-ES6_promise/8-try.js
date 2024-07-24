37aeaba235b24a67808740aea46ebd84338025fa
NyaxDa <nyaxda@gmail.com>
2024-07-23 18:46:16 -0700

    Added File 8

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,10 +1,6 @@
 export default function divideFunction(numerator, denominator) {
-  try {
     if (denominator === 0) {
-      throw new Error('cannot divide by 0');
+        throw new Error('cannot divide by 0');
     }
     console.log(numerator / denominator);
-  } catch (err) {
-    console.error(err.message);
-  }
-}
+    }
