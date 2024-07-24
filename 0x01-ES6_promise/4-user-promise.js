00985aeca20d5ab096d991f792e148cf1674091d
NyaxDa <nyaxda@gmail.com>
2024-07-23 17:09:49 -0700

    Esling Fix on File 4

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,13 +1,13 @@
 export default function signUpUser(firstName, lastName) {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            const person = {
-                firstName: firstName,
-                lastName: lastName,
-            };
-            resolve(person);
-        }, 1000);
-    }).then((person) => {
-        console.log(`${person.firstName} ${person.lastName}`);
-    });
-  }
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const person = {
+        firstName,
+        lastName,
+      };
+      resolve(person);
+    }, 1000);
+  }).then((person) => {
+    console.log(`${person.firstName} ${person.lastName}`);
+  });
+}
