a37b1d1f83db42bed56cfb79157d168c62c1bd77
NyaxDa <nyaxda@gmail.com>
2024-07-25 16:25:37 -0700

    Added Files 10

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,24 @@
+const _brand = Symbol('brand');
+const _motor = Symbol('motor');
+const _color = Symbol('color');
+
+export default class Car {
+  constructor(brand, motor, color) {
+    if (typeof brand !== 'string') {
+      throw TypeError('Brand must be a string.');
+    }
+    if (typeof motor !== 'string') {
+      throw TypeError('Motor must be a string.');
+    }
+    if (typeof color !== 'string') {
+      throw TypeError('Color must be a string.');
+    }
+    this[_brand] = brand;
+    this[_motor] = motor;
+    this[_color] = color;
+  }
+
+  cloneCar() {
+    return new this.constructor(this[_brand], this[_motor], this[_color]);
+  }
+}
