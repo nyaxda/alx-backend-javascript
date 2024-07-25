62371cb134abb5955de759c507cdac35838adcd9
NyaxDa <nyaxda@gmail.com>
2024-07-25 14:14:09 -0700

    Added Files 6

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,20 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    if (typeof floors !== 'number') {
+      throw TypeError('Floors must be in Numbers');
+    }
+    super(sqft);
+    this._floors = floors;
+  }
+
+  // getter for floors
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors.`;
+  }
+}
