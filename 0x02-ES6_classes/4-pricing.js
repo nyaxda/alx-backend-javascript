ed3ece6c092373e9f2779c82e13e429a1a233ccb
NyaxDa <nyaxda@gmail.com>
2024-07-25 09:01:40 -0700

    Added Files 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,57 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // getter for amount
+  get amount() {
+    return this._amount;
+  }
+
+  // setter for amount
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  // getter for currency
+  get currency() {
+    return this._currency;
+  }
+
+  // setter for currency
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('ConversionRate must be a number');
+    }
+
+    // return amount multiplied by the conversion rate
+    return amount * conversionRate;
+  }
+}
