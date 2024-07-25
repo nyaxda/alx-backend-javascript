942004430a726348e0b328ffe405e0ed43896190
NyaxDa <nyaxda@gmail.com>
2024-07-25 16:08:54 -0700

    Codefix on File 9

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -16,12 +16,11 @@ export class HolbertonClass {
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-export class StudentHolberton extends HolbertonClass {
-  constructor(year, location, firstName, lastName) {
-    super(year, location);
+export class StudentHolberton {
+  constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
-    this._holbertonClass = new HolbertonClass(year, location);
+    this._holbertonClass = holbertonClass;
   }
 
   get fullName() {
