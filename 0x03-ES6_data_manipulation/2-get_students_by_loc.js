9f49da05e8b5bda8757b9f037c848981c4396bc4
NyaxDa <nyaxda@gmail.com>
2024-07-29 14:13:36 -0700

    Errorfix on File 2

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,6 @@
 export default function getStudentsByLocation(students, city) {
+  if (!Array.isArray(students)) {
+    throw new Error('Students is not an array');
+  }
   return students.filter((student) => student.location === city);
 }
