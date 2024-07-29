c5533fad40201e66701a62fad7e152c451995ce5
NyaxDa <nyaxda@gmail.com>
2024-07-29 07:18:41 -0700

    Added Files 1 and ErrorFix File 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 export default function getListStudents() {
   const obj1 = { id: 1, name: 'Guillaume', location: 'San Francisco' };
   const obj2 = { id: 2, name: 'James', location: 'Columbia' };
-  const obj3 = { id: 3, name: 'Serena', location: 'San Francisco' };
+  const obj3 = { id: 5, name: 'Serena', location: 'San Francisco' };
 
   const myArray = [obj1, obj2, obj3];
 
