605e00d8f3a28d6993bac74aad53ae13948c60d7
NyaxDa <nyaxda@gmail.com>
2024-07-29 14:11:52 -0700

    Errorfix on File 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,6 @@
 export default function getStudentIdsSum(students) {
+  if (!Array.isArray(students)) {
+    throw new Error('Students is not an array');
+  }
   return students.reduce((acc, cur) => acc + cur.id, 0);
 }
