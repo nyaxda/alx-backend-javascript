56ce81aaff922c1f499644ca718b9e5ae92054a0
NyaxDa <nyaxda@gmail.com>
2024-07-29 16:29:28 -0700

    Added Files 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
 export default function cleanSet(set, startString) {
   if (!(set instanceof Set)) {
-    throw new Error('Input is not a set');
+    return '';
   }
   if (typeof startString !== 'string') {
-    throw new Error('Start string is not a string');
+    return '';
   }
   if (startString === '') {
     return '';
