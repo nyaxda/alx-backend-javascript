76d764bf4520ba16a5759ad7cac31b3d453f89fa
NyaxDa <nyaxda@gmail.com>
2024-07-29 07:07:21 -0700

    Added Files 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+export default function getListStudents() {
+  const obj1 = { id: 1, name: 'Guillaume', location: 'San Francisco' };
+  const obj2 = { id: 2, name: 'James', location: 'Columbia' };
+  const obj3 = { id: 3, name: 'Serena', location: 'San Francisco' };
+
+  const myArray = [obj1, obj2, obj3];
+
+  return myArray;
+}
