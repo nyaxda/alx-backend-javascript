f2b3f30575ab6bc5f35834b517703129bd698693
NyaxDa <nyaxda@gmail.com>
2024-07-30 06:25:34 -0700

    Refactor

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,34 +1,53 @@
+// export default function updateStudentGradeByCity(students, city, newGrades) {
+// //   // checking if the arguments are of the right types
+//   if (!Array.isArray(students)) {
+//     throw new Error('Students is not an array');
+//   }
+//   if (typeof city !== 'string') {
+//     throw new Error('City is not a string');
+//   }
+//   if (!Array.isArray(newGrades)) {
+//     throw new Error('New grades is not an array');
+//   }
+//   // filtering students by the specified city
+//   const studentsMapped = students.filter((student) => student.location === city);
+
+//   // Mapping through the filtered students to update their grades
+//   return studentsMapped.map((student) => {
+//     // Find the new grade for the student
+//     const aGrade = newGrades.find((grade) => grade.studentId === student.id);
+
+//     // if no grade is found, set the grade to 'N/A
+//     if (!aGrade) {
+//       return {
+//         ...student,
+//         grade: 'N/A',
+//       };
+//     }
+
+//     // return the student object with the updated grade
+//     return {
+//       ...student,
+//       grade: aGrade.grade,
+//     };
+//   });
+// }
+
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  // checking if the arguments are of the right types
-  if (!Array.isArray(students)) {
-    throw new Error('Students is not an array');
-  }
-  if (typeof city !== 'string') {
-    throw new Error('City is not a string');
-  }
-  if (!Array.isArray(newGrades)) {
-    throw new Error('New grades is not an array');
-  }
-  // filtering students by the specified city
-  const studentsMapped = students.filter((student) => student.location === city);
+  const defaultGrade = { grade: 'N/A' };
 
-  // Mapping through the filtered students to update their grades
-  return studentsMapped.map((student) => {
-    // Find the new grade for the student
-    const aGrade = newGrades.find((grade) => grade.studentId === student.id);
-
-    // if no grade is found, set the grade to 'N/A
-    if (!aGrade) {
-      return {
-        ...student,
-        grade: 'N/A',
-      };
-    }
-
-    // return the student object with the updated grade
-    return {
-      ...student,
-      grade: aGrade.grade,
-    };
-  });
+  if (students instanceof Array) {
+    return students
+      .filter((student) => student.location === city)
+      .map((student) => ({
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: (newGrades
+          .filter((grade) => grade.studentId === student.id)
+          .pop() || defaultGrade).grade,
+      }));
+  }
+  return [];
 }
+
