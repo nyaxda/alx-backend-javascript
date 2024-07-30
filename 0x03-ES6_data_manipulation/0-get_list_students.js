78c0011f61d0d20a748107e9a81175fd1f500eb7
NyaxDa <nyaxda@gmail.com>
2024-07-30 06:11:35 -0700

    refactor 2

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -12,10 +12,10 @@
 //   return myArray;
 // }
 
-const getListStudentIds = () => [
+const getListStudents = () => [
   { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
   { id: 2, firstName: 'James', location: 'Columbia' },
   { id: 5, firstName: 'Serena', location: 'San Francisco' },
 ];
 
-export default getListStudentIds;
+export default getListStudents;
