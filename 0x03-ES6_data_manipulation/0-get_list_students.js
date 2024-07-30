2214c12d6e8cc9998738e21da28a6ebda3c8d32a
NyaxDa <nyaxda@gmail.com>
2024-07-30 06:16:21 -0700

    refactor 3

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,21 +1,7 @@
-/**
- * Retrieves a list of students.
- * @returns {Array} An array of student objects.
- */
-// export default function getListStudents() {
-//   const obj1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
-//   const obj2 = { id: 2, firstName: 'James', location: 'Columbia' };
-//   const obj3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
-
-//   const myArray = [obj1, obj2, obj3];
-
-//   return myArray;
-// }
-
 const getListStudents = () => [
-  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-  { id: 2, firstName: 'James', location: 'Columbia' },
-  { id: 5, firstName: 'Serena', location: 'San Francisco' },
-];
-
-export default getListStudents;
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+  
+  export default getListStudents;
