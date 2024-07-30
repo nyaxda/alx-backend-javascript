e19b9f6e508f9645b58ec49e00fd52e373690a82
NyaxDa <nyaxda@gmail.com>
2024-07-30 06:44:32 -0700

    Code Refactor File 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,24 @@
+// export default function cleanSet(set, startString) {
+//   if (!set || !startString || !(set instanceof Set) ||
+// typeof startString !== 'string' || startString === '') {
+//     return '';
+//   }
+//   const myNewSet = [...set].filter((x) => (x.startsWith(startString)));
+//   return myNewSet.map((x) => x.replace(startString, '')).join('-');
+// }
 export default function cleanSet(set, startString) {
-  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string' || startString === '') {
+  const parts = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
-  const myNewSet = [...set].filter((x) => (x.startsWith(startString)));
-  return myNewSet.map((x) => x.replace(startString, '')).join('-');
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
+    }
+  }
+  return parts.join('-');
 }
